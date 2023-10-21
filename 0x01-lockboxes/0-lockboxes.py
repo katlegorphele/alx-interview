72ca2c19d3c24f485f0187fedf77cfd77d98851b
Kat-Devs <katlegophele95@gmail.com>
2023-10-21 10:05:12 +0200

    lockboxes revised

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,19 +4,29 @@
 Method that determines if all boxes can be opened
 '''
 
-from typing import Any, Union, Mapping, TypeVar
+from typing import List
 
-T = TypeVar('T')
-NoneType = TypeVar('NoneType', None, None)
-R = Union[Any, T]
-D = Union[T, None]
+def canUnlockAll(boxes: List[list]) -> bool:
+    '''Check if all boxes can be unlocked'''
 
+    unlocked = {}
 
-def safely_get_value(dct: Mapping, key: Any, default: D) -> R:
-    '''
-    Returns the value
-    '''
-    if key in dct:
-        return dct[key]
-    else:
-        return default
+    try:
+        empty_index = boxes.index([])
+        boxes[empty_index] = [0]
+    except Exception:
+        pass
+
+    box_num = [x for x in range(len(boxes))]
+
+    unlocked[0] = 'unlocked'
+    for i in range(len(boxes) - 1):
+        for k in range(len(boxes[i])):
+            if boxes[i][k] in box_num:
+                unlocked[boxes[i][k] = 'unlocked'
+
+    opened = unlocked.values()
+
+    if len(opened) != len(boxes):
+        return False
+    return True
