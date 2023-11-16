131fbb39ac26c82224a35c68ed2957e7ad3ffb30
Kat-Devs <katlegophele95@gmail.com>
2023-11-16 22:37:13 +0200

    Minor changes to file

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 
-def rotate_2d_matrix(matrix):
+def rotate_2d_matrix(matrix: list) -> None:
     """
     Function that rotates a 2D matrix 90 degrees clockwise
     in place
+
+    Args:
+        matrix: 2D matrix
+
+    Returns:
+        Nothing, modifies matrix in place
     """
 
     # Get the length of the matrix
