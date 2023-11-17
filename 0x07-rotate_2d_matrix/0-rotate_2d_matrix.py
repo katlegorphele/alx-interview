afad2bc663caa2d0fa9652780e590448e78ab0cf
Katlego R. Phele <113664624+katlegorphele@users.noreply.github.com>
2023-11-17 09:07:42 +0200

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -15,10 +15,16 @@ def rotate_2d_matrix(matrix: list) -> None:
 
     # Get the length of the matrix
     n: int = len(matrix)
-    temp: list = []
-    tmp: list = list(matrix)
     for i in range(n):
-        for j in range(n - 1, -1, -1):
-            temp.append(tmp[j][i])
-        matrix[i] = temp
-        temp = []
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for row in matrix:
+        row.reverse()
+    # temp: list = []
+    # tmp: list = list(matrix)
+    # for i in range(n):
+    #     for j in range(n - 1, -1, -1):
+    #         temp.append(tmp[j][i])
+    #     matrix[i] = temp
+    #     temp = []
