93e558d01d4461e9e1dee0d34211dc766d71b505
Kat-Devs <katlegophele95@gmail.com>
2023-11-30 21:09:48 +0200

    0x09 complete

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,16 +10,24 @@ def island_perimeter(grid):
     '''
     
     perimeter = 0
+    # Iterate through grid to get rows
     for row in range(len(grid)):
+        # Iterate through each number in each row
         for col in range(len(grid[row])):
+            # Check if number is 1
             if grid[row][col] == 1:
-                if row == 0 or grid[row - 1][col] == 0:
+                # check if edge or 0 above
+                if row == 0 or grid[row - 1][col] == 0: 
                     perimeter += 1
-                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                # check if edge or 0 below
+                if row == len(grid) - 1 or grid[row + 1][col] == 0: 
                     perimeter += 1
+                # check if edge or 0 left
                 if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 1
+                # check if edge or 0 right
                 if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
+
 
